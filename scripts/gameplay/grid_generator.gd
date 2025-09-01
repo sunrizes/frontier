fe231fb18ee3a61b83abfbf0c94112b417f84c6c
sunrizes <sunrizes99@gmail.com>
2025-09-01 02:31:01 +0200

    Grid generator test

--- scripts/gameplay/grid_generator.gd
+++ scripts/gameplay/grid_generator.gd
@@ -1,3 +1,13 @@
 extends Node
 
+@export var tile: PackedScene
 @export var board_settings: BoardSettings
+
+func _ready() -> void:
+	populate_board(board_settings.board_layout)
+
+func populate_board(tile_positions: Array[Vector3]) -> void:
+	for pos in tile_positions:
+		var new_tile = tile.instantiate()
+		new_tile.position = pos
+		add_child(new_tile)
